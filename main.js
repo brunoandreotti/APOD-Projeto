514f4ee02454c331cc1f521867c491018d7969fe
Bruno Andreotti <brunoandreotti@outlook.com>
2021-12-10 17:46:47 -0300

    tratamento-de-erros

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ $('#button').click(function () {
   const key = 'AFypob2tIvNzSf7g5LgNMqy0BnqbTyG6QgoLe3Yd'
 
   if (data) {
-    url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`;
+    url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`
   } else {
-    url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+    url = `https://api.nasa.gov/planetary/apod?api_key=${key}`
   }
 
   $.ajax({
@@ -15,23 +15,31 @@ $('#button').click(function () {
     success: function (response) {
       console.log(response)
 
-      if(response.media_type == 'image'){
-        $('#picImg').removeClass('visibility')
-        $('#videoImg').addClass('visibility')
-      } else if (response.media_type == 'video') {
-        $('#videoImg').removeClass('visibility')
-        $('#picImg').addClass('visibility')
-      }
-
-      $('#picTitle').text(response.title)
-      $('#picImg').attr('src', `${response.url}`)
-      $('#videoImg').attr('src', `${response.url}`)
-      $('#picDesc').text(response.explanation)
+      exibeConteudo(response)
+    },
+    error: function() {
+      alert('Selecione uma data valida! As imagens estão disponíveis desde 16/06/1995 até o dia de hoje!')
     }
   })
 })
 
+function exibeConteudo(response) {
+
+  let image = $('#picImg')
+  let video = $('#videoImg')
+
+  if (response.media_type == 'image') {
+    image.removeClass('visibility')
+    video.addClass('visibility')
+  } else if (response.media_type == 'video') {
+    video.removeClass('visibility')
+    image.addClass('visibility')
+  }
+
+  $('#picTitle').text(response.title)
+  image.attr('src', `${response.url}`)
+  video.attr('src', `${response.url}`)
+  $('#picDesc').text(response.explanation)
+}
 
 $('#button').click()
-
-
